676c326baf867723702ad42118bb949d5455cb6d
Tian Kwock <tiankwock@Tians-MacBook-Air.local>
2023-10-25 18:08:44 -0400

    decoder

---
+++ lab6.py
@@ -0,0 +1,10 @@
+def decode(password):
+    password = list(password)
+    for element in range(len(password)):
+        password[element] = (int(password[element]) - 3)
+        if password[element] < 0:
+            password[element] = 10 + password[element]
+    password = ''.join(map(str, password))
+    return password
+
+print(decode('45678888'))
